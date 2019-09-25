a987d3b024416072530c6164b31704c55c2b89c8
IgmarH <35600123+IgmarH@users.noreply.github.com>
2019-09-25 11:52:21 +0200

    Add files via upload
    v2

---
+++ challange1.php
@@ -0,0 +1,9 @@
+<?php
+
+$boodschap = "aardappel";
+
+$a = "aardappelen";
+
+echo "ik moet vandaag $a kopen.";
+
+?>
---
+++ opdracht2.php
@@ -0,0 +1,4 @@
+<?php
+$boodschappen = ["aardappelen", "aardbeien", "3 pakken melk","yoghurt"];
+
+echo "Welkom";
